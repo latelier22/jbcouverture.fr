ea73f6004a36a85ce77d0a3dbde5e1dffe6f1c5b
Cyrille LE CORRE <latelier.cyrille@gmail.com>
2024-03-04 18:22:02 +0100

    initial commit

---
+++ app/Title.jsx
@@ -0,0 +1,23 @@
+
+const Section = ({title}) => {
+  
+    return (
+     
+       
+  
+  <section className="p-4 bg-indigo-900">
+  <h2 className="text-4xl text-white text-center font-bold ">
+   {title}
+  </h2>
+  
+  </section>
+  
+    );
+  };
+  
+  export default Section;
+  
+  
+  
+  
+  
